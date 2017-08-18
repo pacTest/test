800cbd90d4aa46f1062b8e2efbff36b750dfacfb
Patrik Bogović <patrik.bogovic@hotmail.com>
2017-08-18 10:46:11 +0200

    This is a test

---
+++ test.ts
@@ -0,0 +1,16 @@
+//<reference path='node_modules/@types/node/index.d.ts' />
+
+///<reference path='node_modules\definitely-typed-jquery\jquery.d.ts' />
+///<reference path='node_modules\npmtest\index.d.ts'/>
+
+
+import * as nesto from "npmtest";
+
+import * as http from "http";
+
+
+
+module test{
+
+    
+}
